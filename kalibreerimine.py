ce5d9a75152ce5979e07cd5e716000c295fe73df
Conasi <rauno.neito@gmail.com>
2017-10-15 14:27:26 +0300

    Palli tuvastamine ja selle poole liikumine

--- kalibreerimine.py
+++ kalibreerimine.py
@@ -1,7 +1,6 @@
-import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 hsv = None
 
 punktid = list()
@@ -17,23 +16,35 @@ cv2.setMouseCallback('image', mouse_callback)
 while (True):
 
     ret, frame = cap.read()
-
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     cv2.imshow('image', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
 cap.release()
 cv2.destroyAllWindows()
-h=list()
-s=list()
-v=list()
+h = list()
+s = list()
+v = list()
 for i in range(len(punktid)):
     h.append(punktid[i][0])
     s.append(punktid[i][1])
     v.append(punktid[i][2])
-alumine=[min(h),min(s),min(v)]
-ülemine=[max(h),max(s),max(v)]
-print(alumine,ülemine)
+parameetrid = [min(h), min(s), min(v), max(h), max(s), max(v)]
+nimi = ""
+while nimi == "":
+    objekt = input("Kas palli(p), korvi(k) või väljaku(v) parameetrid? ")
+    if objekt == "p":
+        nimi = "pall.txt"
+    elif objekt == "k":
+        nimi = "korv.txt"
+    elif objekt == "v":
+        nimi = "valjak.txt"
+    else:
+        print("Vale sisestus, proovi uuesti.")
+fail = open(nimi, "w")
+for i in parameetrid:
+    fail.write(str(i) + ",")
+fail.close()
--- palli tuvastamine.py
+++ palli tuvastamine.py
@@ -1,36 +1,72 @@
 import cv2
 import numpy as np
-lahti=open("parameetrid.txt","r").readline().split(",")
-p=[]
+import serial
+
+con = serial.Serial("COM3", baudrate=115200, timeout=0.8, dsrdtr=True)
+pall = open("pall.txt", "r").readline().split(",")
+korv = open("korv.txt", "r").readline().split(",")
+p = []
+k = []
 for i in range(6):
-    p.append(int(lahti[i]))
+    p.append(int(pall[i]))
+    k.append(int(korv[i]))
 
 cap = cv2.VideoCapture(1)
 while True:
-
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    alumine = np.array([p[0], p[1], p[2]], np.uint8)
-    ülemine = np.array([p[3], p[4], p[5]], np.uint8)
-    mask = cv2.inRange(hsv, alumine, ülemine)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    P_alumine = np.array([p[0], p[1], p[2] - 20], np.uint8)
+    p_ülemine = np.array([p[3], p[4], p[5] + 20], np.uint8)
+    k_alumine = np.array([k[0], k[1], k[2]], np.uint8)
+    k_ülemine = np.array([k[3], k[4], k[5]], np.uint8)
+    p_mask = cv2.inRange(hsv, P_alumine, p_ülemine)
+    k_mask = cv2.inRange(hsv, k_alumine, k_ülemine)
+    res1 = cv2.bitwise_and(frame, frame, mask=p_mask)
+    res2 = cv2.bitwise_and(frame, frame, mask=k_mask)
     cv2.imshow('frame', frame)
-    cv2.imshow('res', res)
+    cv2.imshow('res1', res1)
+    # cv2.imshow('res2', res2)
+    p_cnts = cv2.findContours(p_mask.copy(), cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)[-2]
+    p_center = None
+    if len(p_cnts) == 0:
+        con.write(("sd10:10:10:0" + "\n").encode())
+    if len(p_cnts) > 0:
+        cp = max(p_cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(cp)
+        Mp = cv2.moments(cp)
+        try:
+            p_center = (int(Mp["m10"] / Mp["m00"]), int(Mp["m01"] / Mp["m00"]))
+            if p_center[0] < 290:
+                print("Vasakule")
+                con.write(("sd10:10:10:0" + "\n").encode())
+            elif p_center[0] > 350:
+                print("Paremale")
+                con.write(("sd-10:-10:-10:0" + "\n").encode())
+            elif p_center[0] in range(290, 350):
+                print("Keskel")
+                con.write(("sd:-10:10:0:0" + "\n").encode())
+        except:
+            p_center = None
+            # liigub pallini, saab selle kätte
+            # prioriteet=1
+    # #while prioriteet==1:
+    #     k_cnts = cv2.findContours(k_mask.copy(), cv2.RETR_EXTERNAL,
+    #                           cv2.CHAIN_APPROX_SIMPLE)[-2]
+    #     k_center = None
+    #
+    #     if len(k_cnts) > 0:
+    #         ck = max(k_cnts, key=cv2.contourArea)
+    #         ((x, y), radius) = cv2.minEnclosingCircle(ck)
+    #         Mk = cv2.moments(ck)
+    #         try:
+    #             k_center = (int(Mk["m10"] / Mk["m00"]), int(Mk["m01"] / Mk["m00"]))
+    #         except:
+    #             k_center = None
+    #         # pöörab korvi poole, valib vastavalt kaugusele jõu (ilmselt distance= x ja siis
+    #         # järgmine eraldi asi,mis käivitub). Kui tehtud siis
+    #         # prioriteet=0
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
-
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print(center)
         break
 cv2.destroyAllWindows()
